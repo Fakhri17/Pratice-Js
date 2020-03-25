ba116c5afab25a228bfbe61cc136f04b964aced0
Fakhri Alauddin <fakhricoffe@gmail.com>
2020-03-25 09:34:28 +0700

    first commit file

---
+++ luas_lingkaran.js
@@ -0,0 +1,13 @@
+var jari_jari = 14;
+var phi;
+var luas;
+// Buat ngecek apakah jari2 bisa dibagi 7 atau tidak
+if( jari2%7 == 0 ){
+    phi = 22/7;
+}
+else{
+	phi = 3.14;
+}
+
+luas = phi*jari2*jari2;
+console.log(luas);
